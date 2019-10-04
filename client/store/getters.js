8b1356e2717c66bce8bba9b33c83b48a6650d3da
Sarna <kamilsupera@interia.pl>
2019-10-04 18:10:00 +0200

    Update tests

--- client/store/getters.js
+++ client/store/getters.js
@@ -1,4 +1,8 @@
 // Store all getters used in project
 export default {
+    /**
+     * Get token for authorization process.
+     * @params {boolean} token.
+     */
     authorizedGrant: ({ token }) => !!token,
 }
--- tests/store/mutations.spec.js
+++ tests/store/mutations.spec.js
@@ -1,25 +1,26 @@
 import { expect } from 'chai';
 
 import mutations from '../../client/store/mutations';
-import storeState from '../../client/store/state';
 
 describe('Store mutations', () => {
     describe('logout', () => {
         const { logout } = mutations;
 
-        it('Clear all critical stored data', () => {
+        it('Valid correct logout', () => {
             const state = {
                 token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0NywidXNlcm5hbWUiOiJrYW1pbDk5MiIsImV4cCI6MTU3MDM5NzI3MywiZW1haWwiOiJrYW1pbDk5MkBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU2OTc5MjQ3M30.jI64xqY0K2MVd-eKdAhkEcjUlUFFS4WPhgNzqzK6eJQ',
                 pk: 1,
                 email: 'user@mail.com',
                 username: 'user',
             };
+
             logout(state);
+
             expect(
-                storeState.token,
-                storeState.pk,
-                storeState.email,
-                storeState.username).to.null;
+                state.token,
+                state.pk,
+                state.email,
+                state.username).to.be.null;
         });
     });
 
@@ -28,17 +29,27 @@ describe('Store mutations', () => {
 
         it('Set all user data and token', () => {
             const state = {
-                token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0NywidXNlcm5hbWUiOiJrYW1pbDk5MiIsImV4cCI6MTU3MDM5NzI3MywiZW1haWwiOiJrYW1pbDk5MkBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU2OTc5MjQ3M30.jI64xqY0K2MVd-eKdAhkEcjUlUFFS4WPhgNzqzK6eJQ',
-                pk: 1,
-                email: 'user@mail.com',
-                username: 'user',
+                token: null,
+                pk: undefined,
+                email: undefined,
+                username: undefined,
             };
-            setUser(state);
-            expect(
-                storeState.token,
-                storeState.pk,
-                storeState.email,
-                storeState.username).not.null;
+
+            const data = {
+                token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0NywidXNlcm5hbWUiOiJrYW1pbDk5MiIsImV4cCI6MTU3MDM5NzI3MywiZW1haWwiOiJrYW1pbDk5MkBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU2OTc5MjQ3M30.jI64xqY0K2MVd-eKdAhkEcjUlUFFS4WPhgNzqzK6eJQ',
+                user: {
+                    pk: 1,
+                    username: 'user',
+                    email: 'user@gamil.com',
+                }
+            };
+
+            setUser(state, { data });
+
+            expect(state.token).to.equal(data.token);
+            expect(state.pk).to.equal(data.user.pk);
+            expect(state.username).to.equal(data.user.username);
+            expect(state.email).to.equal(data.user.email);
         });
     });
 });
